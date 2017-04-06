6f7af7b637b9a1739f07d598b31f658f969ff7cc
Carter Sande <carter.sande@duodecima.technology>
2017-04-06 08:47:15 -0700

    Build PyQt (and get better log visibility)

--- build-bottles.sh
+++ build-bottles.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+source $HOME/.travis/job_stages
+
 for i in $( cat bottles-to-build.txt );
 do
     brew install --only-dependencies --build-bottle cartr/qt4/$i
-    brew install --build-bottle cartr/qt4/$i
+    travis_wait 90 brew install --build-bottle cartr/qt4/$i
     brew bottle cartr/qt4/$i
 done
